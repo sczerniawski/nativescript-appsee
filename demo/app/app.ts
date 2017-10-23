48ab132fb9998b61e1956f2db185eafc66fea264
Christopher Chapline <cachapline8@gmail.com>
2017-10-23 16:17:32 -0600

    Update demo app to call start in right location

--- demo/app/app.ts
+++ demo/app/app.ts
@@ -3,7 +3,9 @@ import * as application from 'tns-core-modules/application';
 import * as Appsee from 'nativescript-appsee';
 
 let APPSEE_API_KEY = '<YOUR API KEY HERE>';
-Appsee.setSkipStartValidation(true);
-Appsee.start(APPSEE_API_KEY);
+application.on(application.launchEvent, function(args) {
+    Appsee.setDebug(true);
+    Appsee.start(APPSEE_API_KEY);
+});
 
 application.start({ moduleName: "main-page" });
